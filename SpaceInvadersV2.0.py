6d11e224a20aed898393a89b98f42e16b41213ce
Petr Pavelka <2020-e-pavelka@ad.gyarab.cz>
2022-03-04 09:06:42 +0100

    Space Invaders1.0

---
+++ SpaceInvadersV2.0.py
@@ -0,0 +1,243 @@
+import pygame
+import random
+
+pygame.init()
+pygame.mixer.init()
+
+Jede = True
+Scena = "menu"
+i = 1
+j = 1
+
+Okno_Sirka = 800
+Okno_vyska = 600
+Okno = pygame.display.set_mode((Okno_Sirka, Okno_vyska))
+pygame.display.set_caption("Space Invaders 2.0")
+pygame.display.set_icon(pygame.image.load("Raketka.png"))
+Pozadi_Hry = pygame.image.load("Pozadi1.png").convert()
+Pozadi_Hry = pygame.transform.scale(Pozadi_Hry, (Okno_Sirka, Okno_vyska))
+Start_Button = pygame.Rect(250, 250, 300, 100)
+Styl = pygame.font.Font("freesansbold.ttf", 50)
+Start_Text = Styl.render("START", True, (0, 0, 0))
+Zpet_Button = pygame.Rect(Okno_Sirka / 2 - 20, 2, 20, 20)
+Zpet = pygame.image.load("Zpet.png").convert_alpha()
+Zpet = pygame.transform.scale(Zpet, (20, 20))
+
+#FPS
+FPS = 60
+Hodiny = pygame.time.Clock()
+
+#Nadpis v menu
+StylNadpisu = pygame.font.Font("freesansbold.ttf", 75)
+Nadpis = StylNadpisu.render("SPACE INVADERS", True, (0, 255, 255))
+
+#Hrac/raketka
+Raketka = pygame.image.load("Raketka.png").convert_alpha()
+Raketka = pygame.transform.scale(Raketka, (50, 50))
+RaketkaX = ((Okno_Sirka / 2) - 50)
+RaketkaY = (Okno_vyska - 60)
+RaketkaRychlost = 4
+
+#strela
+Strela = pygame.image.load("Strela.png").convert_alpha()
+Strela = pygame.transform.scale(Strela, (20, 20))
+StrelaX = Okno_Sirka / 2 - 35
+StrelaY = Okno_vyska - 75
+StrelaRychlost = 4
+StatusStrely = "pripravena"
+ZvukVystrelu = pygame.mixer.Sound("Vystrel.wav")
+
+def vystrel(x, y):
+    global StatusStrely
+    StatusStrely = "vystrelila"
+    Okno.blit(Strela, (x, y))
+
+#Hudba
+def HudbaVMenu():
+    MenuHudba = "MenuBackgroundSound.mp3"
+    pygame.mixer.music.load(MenuHudba)
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.1)
+
+def HudbaVeHre():
+    GameBackgroundSound = "GameBackgroundSound.mp3"
+    pygame.mixer.music.load(GameBackgroundSound)
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.1)
+
+def HudbaProhry():
+    hudba = pygame.mixer.Sound("Prohra.mp3")
+    hudba.set_volume(0.1)
+    pygame.mixer.music.stop()
+    hudba.play()
+
+
+#Skore
+Skore = 0
+SkoreX = Okno_Sirka - 150
+SkoreY = 0
+SkoreStyl = pygame.font.Font("freesansbold.ttf", 32)
+
+#Nepřítel
+nepritel = pygame.image.load("Nepritel.png").convert_alpha()
+nepritel = pygame.transform.scale(nepritel, (50, 50))
+class Nepritel(pygame.Rect):
+    def __init__(a, y, width, height):
+        a.x = random.randint(0, Okno_Sirka - 5)
+        a.y = y
+        a.width = width
+        a.height = height
+        a.rychlost = 4
+        a.zmena = 50
+
+    def pohyb(a):
+        a.x -= a.rychlost
+        if a.x <= 0:
+            a.rychlost = -4
+            a.y += a.zmena
+        if a.x >= (Okno_Sirka - 50):
+            a.rychlost = 4
+            a.y += a.zmena
+
+    def tezsi(a):
+        if Skore >= 10:
+            if a.rychlost == -4:
+                a.rychlost = -5
+            if a.rychlost == 4:
+                a.rychlost = 5
+
+    def zasah(a):
+        global Skore, StrelaY, StrelaRychlost, StatusStrely
+        strelaRect = pygame.Rect(StrelaX, StrelaY, 25, 25)
+        vybuch = pygame.mixer.Sound("explosion.wav")
+        if a.colliderect(strelaRect):
+            vybuch.play()
+            Skore += 1
+            a.x = random.randint(0,Okno_Sirka - 5)
+            a.y = 50
+            StrelaY = Okno_vyska - 60
+            StatusStrely = "pripravena"
+
+    def Reset(a):
+        a.x = random.randint(1, Okno_Sirka - 5)
+        a.y = 50
+
+
+N1 = Nepritel(50, 50, 50)
+def Vykreslit():
+    Okno.blit(nepritel, N1)
+
+Prohrals = False
+ZnovuPos = 250
+Znovu_Button = pygame.Rect(ZnovuPos, ZnovuPos, 300, 100)
+Prohra_Text = Styl.render("ZNOVU!", True, (0, 0, 0))
+def Prohra():
+    global N1, Prohrals, ZnovuPos, NejlepsiSkore
+    if N1.y >= Okno_vyska - 100 and N1.y <= Okno_vyska:
+        N1.rychlost = 0
+        HudbaProhry()
+        Prohrals = True
+        ZnovuPos = 250
+    if Prohrals:
+        pygame.draw.rect(Okno, (255, 255, 255), Znovu_Button, 0, 30)
+        Okno.blit(Prohra_Text, (305, 275))
+    if Skore > NejlepsiSkore:
+        NejlepsiSkore = Skore
+        f = open("NejSkore.txt", "w")
+        f.write(str(Skore))
+        f.close()
+
+NejlepsiSkore = 0
+f = open("NejSkore.txt", "r")
+NejlepsiSkore = int(f.read())
+
+while (Jede):
+    Hodiny.tick(FPS)
+    Okno.fill((0, 0, 0))
+    Klavesa = pygame.key.get_pressed()
+
+    if Scena == "menu":
+        pygame.draw.rect(Okno, (170, 0, 255), Start_Button, 0, 30)
+        Okno.blit(Start_Text, (325, 275))
+        Okno.blit(Nadpis, (75, 100))
+
+        if i == 1:
+            HudbaVMenu()
+            i = 2
+
+        if Klavesa[pygame.K_RETURN]:
+            pygame.mixer.music.stop()
+            Scena = "hra"
+            HudbaVeHre()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Jede = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if Start_Button.collidepoint(mouse_pos):
+                    pygame.mixer.music.stop()
+                    Scena = "hra"
+                    HudbaVeHre()
+
+    if Scena == "hra":
+        Okno.blit(Pozadi_Hry, (0, 0))
+        Okno.blit(Raketka, (RaketkaX, RaketkaY))
+        pygame.draw.line(Okno, (255, 255, 255), (0, Okno_vyska - 100), (Okno_Sirka, Okno_vyska - 100), 2)
+        pygame.draw.rect(Okno, (255, 255, 255), Zpet_Button, 0, 3)
+        Okno.blit(Zpet, (Okno_Sirka / 2 - 20, 1))
+
+        Nej_Text = SkoreStyl.render("Nejlepsší skóre: " + str(NejlepsiSkore), True, (255, 255, 255))
+        Okno.blit(Nej_Text, (10, 2))
+
+        skore = SkoreStyl.render("Skóre: " + str(Skore), True, (255, 255, 255))
+        Okno.blit(skore, (SkoreX, SkoreY))
+
+        Vykreslit()
+        N1.zasah()
+        N1.pohyb()
+        N1.tezsi()
+        Prohra()
+
+        if j == 1:
+            HudbaVeHre()
+            j = 2
+
+        if Klavesa[pygame.K_LEFT] and RaketkaX >= 0:
+            RaketkaX -= RaketkaRychlost
+        if Klavesa[pygame.K_RIGHT] and RaketkaX <= (Okno_Sirka - 50):
+            RaketkaX += RaketkaRychlost
+        if Klavesa[pygame.K_SPACE]:
+            if StatusStrely == "pripravena":
+                ZvukVystrelu.play()
+                ZvukVystrelu.set_volume(0.1)
+                StrelaX = RaketkaX
+                vystrel(StrelaX + 15, StrelaY)
+        if Klavesa[pygame.K_ESCAPE]:
+            pygame.mixer.music.stop()
+            Scena = "menu"
+            HudbaVMenu()
+
+        if StrelaY <= 0 - 20:
+            StrelaY = Okno_vyska - 75
+            StatusStrely = "pripravena"
+        if StatusStrely == "vystrelila":
+            vystrel(StrelaX + 15, StrelaY)
+            StrelaY -= StrelaRychlost
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Jede = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if Zpet_Button.collidepoint(mouse_pos):
+                    pygame.mixer.music.stop()
+                    Scena = "menu"
+                    HudbaVMenu()
+                if Znovu_Button.collidepoint(mouse_pos):
+                    HudbaVeHre()
+                    Prohrals = False
+                    N1.Reset()
+                    RaketkaX = (Okno_Sirka / 2) - 50
+
+    pygame.display.flip()
